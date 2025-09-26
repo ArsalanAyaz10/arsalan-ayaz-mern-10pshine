5e6a105a7526778d7e54488da0daac70a64dad40
Arsalan <arsalanayazkps@gmail.com>
2025-09-26 15:51:51 +0500

    Fix backend setup: added package.json and project structure

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+// Connect to database
+connectDB();
