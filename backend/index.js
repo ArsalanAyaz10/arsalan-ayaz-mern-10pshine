f963f03794755d1ced475f7bd754d291b2809ad3
Arsalan <arsalanayazkps@gmail.com>
2025-11-04 22:27:46 +0500

    fixed index file

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 app.use(cookieParser());
-app.use(cors({origin: "http://localhost:3000", credentials: true})); 
+app.use(cors({origin: "http://localhost:5173", credentials: true})); 
 app.use(helmet());
 app.use(morgan("dev"));
 connectDB();
