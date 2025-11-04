0adab0592d974b2554a91ca27d67083737ce5758
Arsalan <arsalanayazkps@gmail.com>
2025-11-05 00:11:39 +0500

    added profile api integration and about page and bug fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-// src/App.tsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import HomePage from "./pages/HomePage";
