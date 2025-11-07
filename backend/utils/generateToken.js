fb50058de4f0265443a5d777a501005d0e9f22f5
Arsalan <arsalanayazkps@gmail.com>
2025-11-07 12:28:11 +0500

    notes test fix

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -14,7 +14,7 @@ const generateAccessToken  = (user)=>{
 const generateRefreshToken = (user) => {
     return jwt.sign(
         { id: user._id },
-        procaess.env.JWT_REFRESH_SECRET || "test-refresh",
+        process.env.JWT_REFRESH_SECRET || 'test-refresh' ,
         { expiresIn: '7d' } 
     );
 };
